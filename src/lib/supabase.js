91c9e4492697ac52e8b0b7bba6039b95bad0afaf
EndermanHD28 <endermanbr666@gmail.com>
2026-05-03 20:46:06 -0300

    Fixed not logging to discor (again)d

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 // This check prevents the "supabaseUrl is required" error during the build
 if (!supabaseUrl || !supabaseAnonKey) {
